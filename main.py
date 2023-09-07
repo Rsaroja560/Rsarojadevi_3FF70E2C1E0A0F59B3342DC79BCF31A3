dcaa813613ba60b7b82772d66a6f6774e5468c60
Rsaroja560 <144329782+Rsaroja560@users.noreply.github.com>
2023-09-07 15:05:37 +0530

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-num =int(input("enter values"))
-def sum(n):
-    if n:=1:
-       return n
-    else :
-      return n+ sum( n-1)
-print("sum is :",sum (num))
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+print(factorial(5))
